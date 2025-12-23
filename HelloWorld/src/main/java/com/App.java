3d577cda68fbe5a6fd5d2416288099b24f2d33a2
Helbrecht <damncrus@gmail.com>
2025-12-23 16:41:12 +0300

    14

--- HelloWorld/src/main/java/com/App.java
+++ HelloWorld/src/main/java/com/App.java
@@ -4,7 +4,7 @@ package com;
  * Hello world!
  */
 public class App {
-    private static final String MESSAGE = "Hello World! CODEBY TEST";
+    private static final String MESSAGE = "Hello World! CODEBY TEST for fail111!!";
     public App() {}
     public static void main(String[] args) {
         System.out.println(MESSAGE);
